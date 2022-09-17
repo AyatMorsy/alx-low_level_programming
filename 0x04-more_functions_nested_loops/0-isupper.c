d3f7c2f53fc9f8aa696a80e57b2a2335ace9e316
AyatMorsy <aya.onlinelearning@gmail.com>
2022-09-17 01:59:09 -0700

    Task0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+supper - Entery point.
+ *
+ * Description: checks for uppercase character.
+ * @c: Character to check.
+ *
+ *Return: 1 whether it is, 0 otherwise
+ */
+ 
+ int _isupper(int c)
+{
+	if ((c>= 'A') && (c<= 'Z'))
+	{
+		return (1);
+	}
+	return (0)
+} 
