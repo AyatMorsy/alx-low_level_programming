fb96aaafa730b15a8fb91ce6c479e3f1d59f929b
AyatMorsy <aya.onlinelearning@gmail.com>
2022-09-18 10:18:45 -0700

    Task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C programme that prints with puts function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main (void)
+{
+	puts("\"programming is like building a multilinual
+			puzzle");
+	return (0);
+} 
